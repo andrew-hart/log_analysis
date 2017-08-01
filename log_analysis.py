79d7cb591a08acde37ab1718aec2fcd29bdffe8e
ahart45 <ahart45@uwo.ca>
2017-07-31 21:07:50 -0400

    Updated query structure and changed query 3

--- log_analysis.py
+++ log_analysis.py
@@ -3,69 +3,76 @@
 import psycopg2
 import datetime
 
+
 class Answers():
     """This class provides the answers to the log analysis assingment"""
 
     def question_1(self):
-        """Answers the first question"""
+        """Answers the first question 'What are the most popular three artiles
+        articles of all time?' and outputs the answer on the screen"""
         DBNAME = "news"
         db = psycopg2.connect(database=DBNAME)
         c = db.cursor()
-        query = ("""select title, count(*) as num from articles, log where 
-                  log.path like CONCAT('%', articles.slug) group by title 
-                  order by num  desc limit 3""")
+        query = ("""SELECT title, count(*) as num
+                    FROM articles, log
+                    WHERE log.path LIKE CONCAT('%', articles.slug)
+                    GROUP by title
+                    ORDER BY num DESC
+                    LIMIT 3""")
         c.execute(query)
         a1 = c.fetchall()
+        db.close()
 
         # Print the query for question 1
         for a in a1:
             print(a[0] + " - " + str(a[1]))
 
-        db.close()
-
     def question_2(self):
-        """Answers the second question"""
+        """Answers the second question 'Who are the most popular article
+        articles authors of all time?' and outputs the answer on the screen"""
         DBNAME = "news"
         db = psycopg2.connect(database=DBNAME)
         c = db.cursor()
-        query = ("""select name, count(*) as num  from authors, articles, log
-                  where authors.id = articles.author and log.path like
-                  CONCAT('%', articles.slug) group by name order by num desc""")
+        query = ("""SELECT name, count(*) as num
+                    FROM authors, articles, log
+                    WHERE authors.id = articles.author and log.path
+                    LIKE CONCAT('%', articles.slug)
+                    GROUP BY name
+                    ORDER BY num DESC""")
         c.execute(query)
         a2 = c.fetchall()
+        db.close()
 
         # Print the query for question 2
         for a in a2:
             print(a[0] + " - " + str(a[1]))
 
-        db.close()
-
     def question_3(self):
-        """Answers the third question"""
+        """Answers the third question 'On which days did more than 1% of
+        requests lead to errors?' and outs the answer on the screen"""
         DBNAME = "news"
         db = psycopg2.connect(database=DBNAME)
         c = db.cursor()
-        c.execute("select * from error_table where results >= 1.0;")
+        c.execute("""SELECT total.date, (100 * errors::float / total) AS
+                     error_percentage
+                     FROM total, errors
+                     WHERE total.date = errors.date
+                     AND (100 * errors::float / total) > 1""")
         a3 = c.fetchall()
+        db.close()
 
         # Print the query for question 3
         for a in a3:
-            date = a[0].strftime('%b %d, %Y')
-            error_percentage = a[1]
-            print(date+ " - " + str(error_percentage)[0:4] + "%")
+            print('{0:%B %d, %Y} - {1:.2f}% errors'.format(a[0], a[1]))
 
-        db.close()
-
-# Create an Answer object and call its methods to display answers
-answer = Answers()
-print("The most popular three articles of all time are:")
-answer.question_1()
-print("")
-print("The most popular authors of all time are:")
-answer.question_2()
-print("")
-print("The days with more than 1% of requests leading to errors are:")
-answer.question_3()
-
-        
-        
+if __name__ == '__main__':
+    # Create an Answer object and call its methods to display answers
+    answer = Answers()
+    print("The most popular three articles of all time are:")
+    answer.question_1()
+    print("")
+    print("The most popular authors of all time are:")
+    answer.question_2()
+    print("")
+    print("The days with more than 1% of requests leading to errors are:")
+    answer.question_3()
